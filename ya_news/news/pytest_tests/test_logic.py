84ee3a76dc4a6e6447921b2a4be40656fd4581e2
Денис <denistischin@mail.ru>
2026-03-08 22:37:55 +0300

    django-testing

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -22,7 +22,9 @@ def test_user_can_create_comment(author_client, news, author):
 
     response = author_client.post(url, data=data)
     assert response.status_code == HTTPStatus.FOUND
-    assert response.url == f'{url}#comments'
+
+    expected_url = reverse('news:detail', args=(news.pk,)) + '#comments'
+    assert response.url == expected_url
 
     comment = Comment.objects.get()
     assert comment.text == data['text']
