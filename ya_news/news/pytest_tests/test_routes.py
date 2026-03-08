7f40cddaba9de51fadab2b017dae6d8bad0492a3
Денис <denistischin@mail.ru>
2026-03-08 13:53:50 +0300

    django-testing

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -17,6 +17,11 @@ def test_pages_available_for_anonymous(client, news):
         response = client.get(url)
         assert response.status_code == HTTPStatus.OK
 
+    logout_url = reverse('users:logout')
+    response = client.post(logout_url)
+    assert response.status_code == HTTPStatus.FOUND
+    assert response.url.startswith(reverse('users:login'))
+
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
