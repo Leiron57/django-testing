c93f6f15ead5ac9bee116bd9acc9c19bd30453c6
Денис <denistischin@mail.ru>
2026-03-09 20:29:38 +0300

    django-testing

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 from news.forms import BAD_WORDS, WARNING
 from news.models import Comment
-from pytest_django.asserts import assertRedirects, assertFormError
+from pytest_django.asserts import assertFormError
 
 
 @pytest.mark.django_db
